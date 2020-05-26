976760deeeb31e8af8f39d68b8ccf22a3868d755
Weslley Braga <weslleybraga@Weslleys-MacBook-Air.local>
2020-05-26 01:24:11 -0500

    newlywed question generator working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import questions from  './questions.json'
+import './main.css'
+const generateNumber = arr => Math.floor((Math.random() * arr.length)) + 1
 
 function App() {
+  const [question, setQuestion] = React.useState('')
+
+  const randomQuestionHandler = () => {
+    const newIndex = generateNumber(questions)
+    setQuestion(questions[newIndex])
+  }
+
+  const clearHandler = () => setQuestion('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div class="main-box"><header className="App-header">
+      <h1>Newlywed Game</h1>
       </header>
+      <main>
+      <div className='random-question'>
+      {question}
+      </div>
+      <button onClick={randomQuestionHandler}>Generate</button>
+      <button onClick={clearHandler}>Clear</button>
+      </main></div>
     </div>
   );
 }
